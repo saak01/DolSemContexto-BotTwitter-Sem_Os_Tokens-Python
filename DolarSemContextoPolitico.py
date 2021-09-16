d3450b482936e895c0bf53d4a01906dfc5b8d7b9
saak01 <joaovictoralvescosta051@gmail.com>
2021-09-16 11:52:39 -0300

    Adicao de todos os materias, tirando os tokens do meu bot.

---
+++ DolarSemContextoPolitico.py
@@ -0,0 +1,68 @@
+from Elementos import *
+from time import sleep
+from tweepy import *
+import tweepy
+from Elementos import *
+
+# Autenticação do Twitter
+auth = tweepy.OAuthHandler("Tokens", "Tokens")
+auth.set_access_token("Tokens", "Tokens")
+#Criando o Objeto da api
+api = tweepy.API(auth)
+
+#Limites de Tweet
+api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+
+
+
+def main():
+    atual=0
+    while True:
+        #loop para o bot não parar
+        semana,limitehor=pausa()
+
+        if semana != 5 and semana != 6:
+            if limitehor > 8 and limitehor < 18:
+                try:
+                    while True:
+                        horario=datas()
+                        dol=cotação()
+                        dol=float(dol)
+                        if dol > atual:
+                            #tw é a variavael com nome tweet é a informação que vai ser postada pelo tweet
+                            tw=float(dol)
+                            #formatação para uma melhor exp
+                            tw1= (f'{tw:.2f}')
+                            #Mudando os tipos primitivos de float para str"
+                            tw1=str(tw1)
+                            #tratamento de str para trocar "." por ","
+                            tw1=tw1.replace('.',',')
+                            #Imprimindo o tweet com as infos recolhidas
+                            api.update_status(f'''Dólar subiu: R$ {tw1} / {horario}
+                                    :(''')
+                            print(f'Tweet Postado as: {horario}')
+                            atual=dol
+                        
+                        if dol < atual:
+                            #tw é a variavael com nome tweet é a informação que vai ser postada pelo tweet
+                            tw=float(dol)
+                            #formatação para uma melhor exp
+                            tw1= (f'{tw:.2f}')
+                            #Mudando os tipos primitivos de float para str"
+                            tw1=str(tw1)
+                            #tratamento de str para trocar "." por ","
+                            tw1=tw1.replace('.',',')
+                            #Imprimindo o tweet com as infos recolhidas
+                            api.update_status(f'''Dólar caiu: R$ {tw1} / {horario}
+                                    :)''')
+                            print(f'Tweet Postado as: {horario}')
+                            atual=dol
+                        if dol == atual:
+                            #pass foi usado para não ocorrer um tipo de flood com o mesmo valor do dolar
+                            pass   
+                        sleep(60*20)
+                except:
+                    pass 
+
+#chamado da main
+main()
---
+++ Elementos.py
@@ -0,0 +1,41 @@
+from datetime import *
+from requests import *
+from bs4 import *
+import requests
+from requests.models import parse_url
+from time import *
+
+def cotação():
+    '''
+    -A função
+    '''
+    while True:
+        res=requests.get('http://economia.awesomeapi.com.br/json/last/USD-BRL')
+        resc=res.json()
+        cotação_dolar = resc["USDBRL"]["bid"]
+        cotação_dolar=float(cotação_dolar)
+        cotação_dolar=f'{cotação_dolar:.2f}'
+        return cotação_dolar
+
+def variação():
+    while True:
+        res=requests.get('http://economia.awesomeapi.com.br/json/last/USD-BRL')
+        resc=res.json()
+        cotação_dolarv = resc["USDBRL"]["varBid"]
+        return cotação_dolarv
+
+def datas():
+    from datetime import datetime
+    
+    hor=datetime.now().strftime('%H:%M')
+    return hor
+
+def pausa():
+    from datetime import datetime
+    
+    hor=datetime.now().strftime('%H')
+    hor=int(hor)
+    semana=datetime.today().weekday()
+    semana= int(semana)
+    return semana,hor
+
